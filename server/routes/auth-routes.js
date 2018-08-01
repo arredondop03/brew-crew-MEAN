46eb7552639b8be475d1888f4da7a89c570b3048
Paola Arredondo <arredondop03@gmail.com>
2018-08-01 12:02:54 -0400

    before pulling request number 14

--- server/routes/auth-routes.js
+++ server/routes/auth-routes.js
@@ -2,6 +2,29 @@
 const express = require('express');
 const router  = express.Router();
 //the rest are things that I installed
+const passport   = require('passport');
+const bcrypt     = require('bcryptjs');
+//user model 
+const User = require('../models/user')
+
+//---------------------Sign Up-------------
+
+router.post('/signup', ())
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------end of Sign Up-------------
+
 
 
 
