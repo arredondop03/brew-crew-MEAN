96a331eb647c9d0106aabf95ab33104469e9818d
RosaRend <ice2meetu@outlook.com>
2018-08-06 10:54:56 -0400

    SignUp Form

--- client/src/app/user/user.component.ts
+++ client/src/app/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule }       from '@angular/forms'
+import { FormsModule }       from '@angular/forms';
 import { AuthService }       from '../services/auth.service';
 
 @Component({
